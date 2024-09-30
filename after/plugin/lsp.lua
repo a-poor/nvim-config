7a2b9792152bc2961961be94e38066d7244b7d32
Austin Poor <45295232+a-poor@users.noreply.github.com>
2024-09-30 13:10:24 -0700

    Updates plugins

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -43,7 +43,7 @@ require('mason-lspconfig').setup({
   -- Replace the language servers listed here
   -- with the ones you want to install
   ensure_installed = {
-    'tsserver',
+    -- 'tsserver',
     'eslint',
     -- 'sumneko_lua',
     'rust_analyzer',
